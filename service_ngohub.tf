b5401027e12bfedc9d2578e0778f7b2d1de1f0eb
Andrei Ioniță <hi@andrei.io>
2023-12-15 14:08:13 +0000

    fix: self service sign up (#10)

--- service_ngohub.tf
+++ service_ngohub.tf
@@ -20,6 +20,10 @@ module "ngohub_cognito" {
   hmac_api_key    = var.ngohub_hmac_api_key
   hmac_secret_key = var.ngohub_hmac_secret_key
 
+  # Settings this to false enables user felf-service sign-up in Cognito
+  # We don't want this for ngohub, but we do for vic
+  allow_admin_create_user_only = true
+
   username_attributes      = ["email"]
   auto_verified_attributes = ["email"]
 
