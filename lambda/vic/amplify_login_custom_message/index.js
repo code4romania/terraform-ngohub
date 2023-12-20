69c98433fb02f2b6a8405aed1f7efb0bc86df65b
Andrew Radulescu <andrew.radulescu@wearetribus.com>
2023-12-20 18:03:36 +0200

    fix: email templates in cognito for VIC + deploy 1.0.4 vic

--- lambda/vic/amplify_login_custom_message/index.js
+++ lambda/vic/amplify_login_custom_message/index.js
@@ -3,18 +3,15 @@
 const { getForgotPasswordEmailTemplate, getForgotPasswordEmailTitle, getInviteUserEmailTemplate, getInviteUserEmailTitle } = require('./email_template_gen');
 
 exports.handler = async (event, context, callback) => {
-  if (event.request.clientMetadata) {
-    ({ appId, appName, appLink, region, environment } = event.request.clientMetadata);
-  }
 
-  const inviteLink = process.env.onghub_frontend_url;
   const contactEmail = process.env.email_contact;
   const emailAssetsUrl = process.env.email_assets_url;
 
-  if (event.triggerSource === 'CustomMessage_AdminCreateUser') {
+
+  if (event.triggerSource === 'CustomMessage_SignUp') {
     event.response = {
       emailSubject: getInviteUserEmailTitle(),
-      emailMessage: getInviteUserEmailTemplate(event.request.usernameParameter, event.request.codeParameter, inviteLink, contactEmail, emailAssetsUrl),
+      emailMessage: getInviteUserEmailTemplate(event.request.usernameParameter, event.request.codeParameter, contactEmail, emailAssetsUrl),
     };
   }
 
