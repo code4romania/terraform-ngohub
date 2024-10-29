4a0b702055b264a40efcea42de2b0f2f1d8d2b6b
Andrei Ioniță <hi@andrei.io>
2024-10-29 09:40:55 +0000

    wip

--- service_vic_pdf_generator.tf
+++ service_vic_pdf_generator.tf
@@ -11,6 +11,8 @@ resource "aws_lambda_function" "pdf_generator" {
   handler          = "index.handler"
   runtime          = "nodejs20.x"
   source_code_hash = data.archive_file.pdf_generator.output_base64sha256
+  memory_size      = 512
+  timeout          = 30
 }
 
 resource "aws_iam_role" "pdf_generator_lambda" {
@@ -26,5 +28,29 @@ data "aws_iam_policy_document" "pdf_generator_lambda_role" {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
+  }
+}
+
+resource "aws_iam_role_policy" "pdf_generator_lambda_role" {
+  name   = "s3-access"
+  role   = aws_iam_role.pdf_generator_lambda.id
+  policy = data.aws_iam_policy_document.pdf_generator_lambda_s3.json
+}
+
+data "aws_iam_policy_document" "pdf_generator_lambda_s3" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:GetObjectAcl",
+      "s3:PutObjectAcl",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      module.vic_s3_private.arn,
+      "${module.vic_s3_private.arn}/*",
+    ]
   }
 }
