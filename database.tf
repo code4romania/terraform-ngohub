7f17a09e9bd7cc9b7836c2c9e5b0fa7c31e395f0
Andrei Ioniță <hi@andrei.io>
2025-05-15 12:24:08 +0100

    increase memory allocation (#133)

--- database.tf
+++ database.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "main" {
   iam_database_authentication_enabled = true
 
   engine                      = "postgres"
-  engine_version              = "13.15"
+  engine_version              = "13.16"
   allow_major_version_upgrade = false
   auto_minor_version_upgrade  = true
 
