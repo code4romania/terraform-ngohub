112e608aaa81768ffc66f63c2958070ee4a2042f
Andrei Ioniță <hi@andrei.io>
2026-02-27 13:30:26 +0000

    update postgres (#140)

--- database.tf
+++ database.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "main" {
   iam_database_authentication_enabled = true
 
   engine                      = "postgres"
-  engine_version              = "13.16"
+  engine_version              = "17.6"
   allow_major_version_upgrade = false
   auto_minor_version_upgrade  = true
 
